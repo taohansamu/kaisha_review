dcfec668128298ea5a199f66f3d45c601d7389cb
taohansamu <dinhtao995>
2017-10-02 18:16:20 +0700

    init all model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :skills
+  resources :types
+  resources :companies
   devise_for :users
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
