05167764ffd66130c78e196c5aa70a00c3152a79
levanthuan <levanthuankmhd@gmail.com>
2017-10-04 21:34:42 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :types
   resources :companies
   devise_for :users
-  root to: "home#index"
+  root to: "companies#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
