866654cef0d1ee3b83b5665f3188d4a1e672bca7
taohansamu <dinhtao995>
2017-10-05 00:11:01 +0700

    config email to send to confirm after signup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,12 +27,23 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
   #TODO: config email:
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {  
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mail.google.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "taohansamu995@gmail.com",
+    password: "xxxxxx"
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
