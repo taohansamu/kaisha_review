4aa626fcf76e786c10f0a3dc721f93febbbcba3d
taohansamu <dinhtao995>
2017-11-01 19:53:33 +0700

    before pull code deviltruong-truong/edit_ui4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,4 @@
 //= require bootstrap-sass-official/assets/javascripts/bootstrap
 //= require trix
 //= require bootstrap-hover-dropdown/bootstrap-hover-dropdown
+//= require upload-image-preview/jquery.upload_preview.min.js
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
+# Use npm
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # Use bower
 Rails.application.config.assets.paths << Rails.root.join("vendor","assets","bower_components")
 
